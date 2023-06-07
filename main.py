9b0536f5b0e1092ad672897a0eb8cc231ee968ea
VictorMauroy <mauroy.victor@gmail.com>
2023-06-07 15:50:40 +0200

    Feat: :sparkles: add a function to create a tree with branchs from the values of a dictionary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from random import randint
 from path_calculation import calculate_distance
+from tree import Tree
 
 # dict of str : tuple
 planet_positions = {
@@ -12,7 +13,9 @@ planet_positions["a_point"] = (randint(-500, 500), randint(-500, 500))
 planet_positions["b_point"] = (randint(-500, 500), randint(-500, 500))
 planet_positions["c_point"] = (randint(-500, 500), randint(-500, 500))
 
-print(f"A point is equal to : {planet_positions['a_point']}")
-print(f"B point is equal to : {planet_positions['b_point']}")
+root_tree = Tree('origin', planet_positions['origin'], )
+root_tree.create_tree_from_dictionary(planet_positions)
 
-print(f"{calculate_distance(planet_positions['a_point'],planet_positions['b_point'])}")
+# print(f"A point is equal to : {planet_positions['a_point']}")
+# print(f"B point is equal to : {planet_positions['b_point']}")
+# print(f"{calculate_distance(planet_positions['a_point'],planet_positions['b_point'])}")
--- tree.py
+++ tree.py
@@ -1,6 +1,52 @@
 class Tree :
-    def __init__(self) -> None:
-        pass
+#region Initialization, add child and check leaf
+    def __init__(self, name:str, coordonate:tuple[int, int], parent = None) -> None:
+        self.name = name
+        self.coordonates = coordonate
+        self.child_branchs:list[Tree] = []
+        self.parent_branch = parent
+
+    def add_child(self, name:str, value:tuple, parent) -> None :
+        """Initialize and add a child to the current branch"""
+        new_child = Tree(name, value, parent)
+        self.child_branchs.append(new_child)
+
+    def is_leaf(self) -> bool :
+        """Whether the current tree has childs branchs or not"""
+        return not self.child_branchs
     
+    def is_root(self) -> bool :
+        """Whether the current tree is the root or not"""
+        return not self.parent != None
+#endregion
+
+    def get_depth(self) -> int :
+        """Return the count of childs in the current tree"""
+        if self.is_leaf() :
+            return 1
+        
+        depth_list = [childBranch.get_depth() for childBranch in self.child_branchs]
+        return max(depth_list) + 1
+
     def get_all_leafs(self, tree_of_paths) -> list :
         """Check a given tree and return all its leafs (last childs without childs)"""
+
+    def create_tree_from_dictionary(self, positions_dict:dict) :
+        """Create one child for each element of the dictionary different of us and remove ourself
+
+        Args:
+            positions_dict (dict): Dictionary with the remaining position to create as a child branch.
+        """
+        saved_pos_dict = positions_dict.copy()
+        if self.name in saved_pos_dict : # Check if 'key'(self.name) exists in dictionary
+            saved_pos_dict.pop(self.name)
+        
+        # Check if the dictionary is empty !
+        if not saved_pos_dict :
+            return
+
+        count = 0
+        for key, value in saved_pos_dict.items() :
+            self.add_child(key, value, self)
+            self.child_branchs[count].create_tree_from_dictionary(saved_pos_dict)
+            count += 1
