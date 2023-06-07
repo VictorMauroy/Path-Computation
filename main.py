d1ea24d072b4a47e5e89b3cca2061c0b220969c4
VictorMauroy <mauroy.victor@gmail.com>
2023-06-07 12:39:55 +0200

    Feat: :sparkles: add a point calculation distance function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from random import randint
+from path_calculation import calculate_distance
 
 # dict of str : tuple
 planet_positions = {
@@ -11,3 +12,7 @@ planet_positions["a_point"] = (randint(-500, 500), randint(-500, 500))
 planet_positions["b_point"] = (randint(-500, 500), randint(-500, 500))
 planet_positions["c_point"] = (randint(-500, 500), randint(-500, 500))
 
+print(f"A point is equal to : {planet_positions['a_point']}")
+print(f"B point is equal to : {planet_positions['b_point']}")
+
+print(f"{calculate_distance(planet_positions['a_point'],planet_positions['b_point'])}")
--- path_calculation.py
+++ path_calculation.py
@@ -6,11 +6,18 @@
 # At the end, we will return the list/dictionary of values with the smallest sum of distances.
 
 from tree import Tree
+import math
 
 def calculate_distance(first_point:tuple, second_point:tuple) -> float :
     """Take two points (vector 2) and return the distance between them (float)"""
+    x1, y1 = first_point
+    x2, y2 = second_point
+    return math.sqrt(
+        (x2 - x1) ** 2 +
+        (y2 - y1) ** 2
+    )
 
-def find_shortest_path(tree_of_paths:Tree) -> tuple(list, float) :
+#def find_shortest_path(tree_of_paths:Tree) -> tuple(list, float) :
     """_summary_
 
     Returns:
@@ -18,10 +25,10 @@ def find_shortest_path(tree_of_paths:Tree) -> tuple(list, float) :
         second, return the sum of the distances finded.
     """
 
-def calculate_path_sum(leaf:Tree) -> float:
+#def calculate_path_sum(leaf:Tree) -> float:
     """Calculate the distance from a leaf and each parents and return the sum of distances
     That function will call multiple time the function 'calculate_distance'"""
 
 # Reçoit une liste comprenant chaque chemin possible (successions de points) et la somme de leurs distances.
-def compare_distance_of_lists(path_finded:list(tuple(list, float))) -> tuple(list, float) :
+#def compare_distance_of_lists(path_finded:list(tuple(list, float))) -> tuple(list, float) :
     """"""
